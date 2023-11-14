af23f6ed29f899a14a51bbadce51309e292e2173
seizaemon <oden@venus.dti.ne.jp>
2023-11-14 22:08:04 +0900

    Reportモデルのテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @test_report = Report.new
+    @test_report.user = users(:one)
+  end
+
+  test 'ログインユーザーがreport作成ユーザーと同じ場合editableがtrueになる' do
+    assert_equal true, @test_report.editable?(users(:one))
+  end
+
+  test 'ログインユーザがreport作成ユーザーと違う場合はeditableがfalseになる' do
+    assert_equal false, @test_report.editable?(users(:two))
+  end
+
+  test 'created_onを呼び出して指定の日時が返る' do
+    @test_report.created_at = '2023-11-11'
+    assert_equal Date.new(2023, 11, 11), @test_report.created_on
+  end
 end
