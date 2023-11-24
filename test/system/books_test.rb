5bc3cac99ee869573287598d76f7cdf0dfaeeaeb
seizaemon <oden@venus.dti.ne.jp>
2023-11-24 22:02:20 +0900

    Bookのsystem testを通した

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -19,32 +19,37 @@ class BooksTest < ApplicationSystemTestCase
 
   test 'should create book' do
     visit books_url
-    click_on 'New book'
+    click_on I18n.t('views.common.new', name: Book.model_name.human)
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in_context
+    click_on I18n.t('helpers.submit.create')
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text I18n.t('controllers.common.notice_create', name: Book.model_name.human)
+    click_on I18n.t('views.common.back', name: Book.model_name.human)
   end
 
   test 'should update Book' do
     visit book_url(@book)
-    click_on 'Edit this book', match: :first
+    click_on I18n.t('views.common.edit', name: Book.model_name.human), match: :first
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in_context
+    click_on I18n.t('helpers.submit.update')
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text I18n.t('controllers.common.notice_update', name: Book.model_name.human)
+    click_on I18n.t('views.common.back', name: Book.model_name.human)
   end
 
   test 'should destroy Book' do
     visit book_url(@book)
-    click_on 'Destroy this book', match: :first
+    click_on I18n.t('views.common.destroy', name: Book.model_name.human), match: :first
 
-    assert_text 'Book was successfully destroyed'
+    assert_text I18n.t('controllers.common.notice_destroy', name: Book.model_name.human)
+  end
+
+  private
+
+  def fill_in_context
+    fill_in I18n.t('activerecord.attributes.book.memo'), with: @new_book.memo
+    fill_in I18n.t('activerecord.attributes.book.title'), with: @new_book.title
   end
 end
